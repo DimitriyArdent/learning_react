3a771aae74ae4cfc68136614e6bed8830d56691a
Dimitriy <dystopyglefa@gmail.com>
2024-03-17 08:28:14 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+ 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const[arr, setArr]= useState(['one', 'two'])
+
+  const[numbers, setNumbers] = useState(['three', 'four'])
+  const[text, setText] = useState('')
+
+   function addNumber(){
+    setArr((prev)=>[...prev,'thousand'])
+   }
+
+   function mergeArrays(n){
+    setArr((prev)=>[...prev,n])
+   }
+   function deleteMe(clickedElement,index){
+      //setArr(arr.filter((element)=> element !=clickedElement))
+     setArr(arr.filter((element, elementIndex)=> elementIndex != index))
+   }
+   function handleChange(text){
+        console.log(text)
+        setText(text)
+   }
+   function handleAddNumber(){
+    setArr((prev)=>[...prev,text])
+    //setText('')
+   }
+   return (
+  <div id='my first tag'>
+        <div  >
+
+
+          {arr.map((el,i)=>(
+
+            <div className='space'>
+                      <div key={i} className='text-color' >   {`${el}    ${i}`}</div>
+                  <button onClick={()=>deleteMe(el,i)}>delete me </button>
+            </div>
+          
+                 
+             
+            
+        ))}
+        <button onClick={()=>addNumber()}>click me to add 'ten'</button>
+
+        <div>
+            {numbers.map((n,i)=>(
+              <div key={i} onClick={()=>mergeArrays(n)} className='numbers'>{n}</div>
+            ))}
+        </div>
+
+                <div className='space'>
+                        <button onClick={()=> handleAddNumber()}>add number</button>
+                        <input value={text} onChange={(e)=>handleChange(e.target.value)}></input>
+                        <div>{text}</div>
+                </div>
+        </div>
+  </div>   
   );
 }
 
