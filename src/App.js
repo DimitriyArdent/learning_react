dc9d6415bf23deba4878f381faa14b1e56a72bae
Dimitriy <dystopyglefa@gmail.com>
2024-03-17 10:40:45 +0200

    2

--- src/App.js
+++ src/App.js
@@ -1,64 +1,60 @@
 import './App.css';
 import { useState } from 'react';
- 
 
 function App() {
-  const[arr, setArr]= useState(['one', 'two'])
+ const [isOpen,setIsOpen] = useState(false)
+ const [val,setVal] = useState('')
+ const [trenary,setTrenary] = useState(true)
+ const [secondTrenary,setSecondTrenary] = useState(false)
+ const [isColor,setIsColor] = useState(false)
+ const [placeholder,setPlaceholder] = useState('')
+const [inlinestyle, setInlinestyle] = useState(0)
+ function handleColorChange(){
+  setIsOpen(!isOpen)
+ }
+ function handleVal(e){
+  setVal(e.target.value)
+ }
+ function handleTrenary(){
+  setTrenary(!trenary)
+ }
+ function handleExperimental(){
 
-  const[numbers, setNumbers] = useState(['three', 'four'])
-  const[text, setText] = useState('')
-
-   function addNumber(){
-    setArr((prev)=>[...prev,'thousand'])
-   }
-
-   function mergeArrays(n){
-    setArr((prev)=>[...prev,n])
-   }
-   function deleteMe(clickedElement,index){
-      //setArr(arr.filter((element)=> element !=clickedElement))
-     setArr(arr.filter((element, elementIndex)=> elementIndex != index))
-   }
-   function handleChange(text){
-        console.log(text)
-        setText(text)
-   }
-   function handleAddNumber(){
-    setArr((prev)=>[...prev,text])
-    //setText('')
-   }
+ }
+ function handleInline(){
+   setInlinestyle(prev => prev+5)
+ }
    return (
-  <div id='my first tag'>
-        <div  >
+    <div className='display'>
+       {val==='red' && <div className='div-app' style={{background:'red'}}>im red div</div>}
+       {val==='purple' && <div className='div-app' style={{background:'purple'}}>im purple div</div>}
+       <button onClick={()=>handleColorChange()}>click me to change color</button>
+       <input value={val} onChange={(e)=>handleVal(e)}></input>
+
+       {trenary ? 
+        <div className='div-app' style={{background:'yellow'}}>im yellow div  </div> :
+           secondTrenary?
+             <div className='div-app' style={{background:'orange'}}>im orange div  </div>:
+             <div className='div-app' style={{background:'green'}}>im green div  </div>
+         }
+
+          <button onClick={()=>handleTrenary()}> trenary button</button>
+
+        
+        <div className={isColor? 'color-a' : 'color-b'} onClick={()=>setIsColor(!isColor)} >im experimental div</div>
+           
+        <div className={`color-a ${placeholder}`} onClick={()=>handleExperimental()} >im experimental div2</div>
+        <input val={placeholder} onChange={(e)=>setPlaceholder(e.target.value)}></input>
 
 
-          {arr.map((el,i)=>(
-
-            <div className='space'>
-                      <div key={i} className='text-color' >   {`${el}    ${i}`}</div>
-                  <button onClick={()=>deleteMe(el,i)}>delete me </button>
-            </div>
-          
-                 
-             
-            
-        ))}
-        <button onClick={()=>addNumber()}>click me to add 'ten'</button>
-
-        <div>
-            {numbers.map((n,i)=>(
-              <div key={i} onClick={()=>mergeArrays(n)} className='numbers'>{n}</div>
-            ))}
-        </div>
-
-                <div className='space'>
-                        <button onClick={()=> handleAddNumber()}>add number</button>
-                        <input value={text} onChange={(e)=>handleChange(e.target.value)}></input>
-                        <div>{text}</div>
-                </div>
-        </div>
-  </div>   
+        <div className={'color-a'} style={{padding:`${inlinestyle}px`}}   >im experimental div3</div>
+         <button onClick={()=>handleInline()}> change experimental div3</button>
+     </div>
+ 
   );
-}
+
+   }
+
+ 
 
 export default App;
